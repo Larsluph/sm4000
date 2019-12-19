467bc44e239df431cf3c6a37af89ce6c778cfe5d
Larsluph <22910497+Larsluph@users.noreply.github.com>
2019-12-19 18:04:39 +0100

    update with relative path pointing to root folder

--- final/client/sm4000.bat
+++ final/client/sm4000.bat
@@ -1,7 +1,7 @@
-start python %~dp0global_controller.py
+start python %~dp0\final\client\global_controller.py
 timeout 1
-start python %~dp0camera.py
+start python %~dp0\final\client\camera.py
 timeout 1
-start python %~dp0sondes.py
+start python %~dp0\final\client\sondes.py
 
 exit
