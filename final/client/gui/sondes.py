10da6c5a2021e0ebf8bdd41d58f78db6147a2835
Larsluph <remarso59@sfr.fr>
2019-12-26 18:01:53 +0100

    GUI sondes.py finished

--- final/client/gui/sondes.py
+++ final/client/gui/sondes.py
@@ -8,79 +8,88 @@ import time
 import keyboard
 import tkinter as tk
 
+def update_window(window):
+  window.update_idletasks()
+  window.update()
+
+def update_debug(window,text,msg):
+  text.config(state=tk.NORMAL)
+  text.insert(tk.END, f"\n{msg}")
+  text.config(state=tk.DISABLED)
+  text.see(tk.END)
+
+  update_window(window)
+
 win = tk.Tk()
 win.title = "GUI_sondes"
 
 tk_vars = {
-    "i":tk.StringVar(),
-    "t":tk.StringVar(),
-    "delta_t":tk.StringVar(),
-    "lvl_val":tk.StringVar(),
-    "lvl_volt":tk.StringVar(),
-    "bat_val":tk.StringVar(),
-    "bat_volt":tk.StringVar(),
-    "pressure":tk.StringVar(),
-    "temp":tk.StringVar(),
-    "depth":tk.StringVar(),
-    "alti":tk.StringVar()
+  "i":tk.IntVar(),
+  "t":tk.DoubleVar(),
+  "delta_t":tk.DoubleVar(),
+  "lvl_val":tk.IntVar(),
+  "lvl_volt":tk.DoubleVar(),
+  "bat_val":tk.IntVar(),
+  "bat_volt":tk.DoubleVar(),
+  "pressure":tk.DoubleVar(),
+  "temp":tk.DoubleVar(),
+  "depth":tk.DoubleVar(),
+  "alti":tk.DoubleVar()
 }
 
-scroll = tk.Scrollbar(win)
-scroll.grid(row=0,column=2)
-debug_screen = tk.Text(win,yscrollcommand=scroll.set)
-debug_screen.grid(row=0,column=0,columnspan=2)
+debug_screen = tk.Text(win,height=10,width=100,state=tk.DISABLED)
+debug_screen.grid(row=1,column=0,columnspan=2)
 
-scroll.config(command=debug_screen.yview)
+i = 0
+for var in list(tk_vars.keys()):
+  tk.Label(win, text=f"{var} value :").grid(row=i+2,column=0)
+  tk.Label(win, textvariable=tk_vars[var]).grid(row=i+2,column=1)
+  i += 1
 
-for i in range(len(tk_vars)):
-    tk.Label(win, text=f"{tk_vars.keys()[i]} value :").grid(row=i+1,column=0)
-    tk.Label(win, textvariable=tk_vars[i]).grid(row=i+1,column=1)
-
-try:
-    ip = ("192.168.137.2","50003")
-
-    tk_vars["status"].set(f"Connecting to {':'.join(ip)}...")
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ip)
-    tk_vars["status"].set("Connected!")
-except:
-    tk_vars["status"].set("unable to connect to host")
-    time.sleep(.5)
-    tk_vars["status"].set("Exiting...")
-    time.sleep(.5)
-    win.destroy()
-    raise SystemExit
+update_window(win)
 
 try:
-    os.system("mkdir sm4000_received_data\\probes_data")
+  ip = ("192.168.137.2",50003)
+
+  update_debug(win,debug_screen,f"Connecting to {':'.join(map(str,ip))}...")
+  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  client.connect(ip)
+  update_debug(win,debug_screen,"Connected!")
 except:
-    pass
+  update_debug(win,debug_screen,"unable to connect to host")
+  update_debug(win,debug_screen,"Exiting...")
+  time.sleep(1)
+  win.destroy()
+  raise SystemExit
+
+try:
+  os.system("mkdir sm4000_received_data\\probes_data")
+except:
+  pass
 
 keyboard.add_hotkey('esc',lambda: exec("global running;running=False"),suppress=False)
-tk_vars["status"].set("waiting for data...")
+update_debug(win,debug_screen,"waiting for data...")
 
-vidnpath = "sm4000_received_data\\probes_data\\" +  time.strftime('sm4000_probes_data_%Y-%m-%d_%H-%M-%S.txt')
+vidpath = "sm4000_received_data\\probes_data\\" + time.strftime('sm4000_probes_data_%Y-%m-%d_%H-%M-%S.txt')
 with open(vidpath,mode='w') as output_file:
-    debug = True
-    running = True
-    while running:
-        try:
-            if debug:
-                tk_vars["status"].set("waiting for data...")
-            data = i,t,delta_t,lvl_val,lvl_volt,bat_val,bat_volt,pressure,temp,depth,alti = client.recv(1024).decode().split(",")
-           
-            for i in "i,t,delta_t,lvl_val,lvl_volt,bat_val,bat_volt,pressure,temp,depth,alti".split(","):
-                tk_vars[i].set(eval(i))
-            output_file.write(data)
-            output_file.flush()
-            win.update_idletasks()
-            win.update()
-        except:
-            tk_vars["status"].set("can't read incoming data")
-            running = False
+  debug = True
+  running = True
+  while running:
+    try:
+      data = i,t,delta_t,lvl_val,lvl_volt,bat_val,bat_volt,pressure,temp,depth,alti = client.recv(1024).decode().split(",")
+
+      for x in "i,t,delta_t,lvl_val,lvl_volt,bat_val,bat_volt,pressure,temp,depth,alti".split(","):
+        tk_vars[x].set(eval(x))
+      output_file.write(data)
+      output_file.flush()
+      update_window(win)
+    except:
+      update_debug(win,debug_screen,"can't read incoming data")
+      running = False
 
 client.close()
-tk_vars["status"].set("Disconnected")
+update_debug(win,debug_screen,"Disconnected")
+time.sleep(1)
 win.destroy()
 
 raise SystemExit
