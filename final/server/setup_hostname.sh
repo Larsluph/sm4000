0b929167e7977c1fbbe53f3458febceba202d183
Larsluph <22910497+Larsluph@users.noreply.github.com>
2020-01-30 21:37:48 +0100

    Create setup_hostname.sh

---
+++ final/server/setup_hostname.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#sudo chmod +x hello-world.sh
+
+sudo apt-get update
+sudo apt-get upgrade # update apt-get
+
+sudo apt-get install avahi-daemon # install avahi to enable hostnames
+
+sudo nano /etc/hosts # edit file with the wanted hostname (default: raspberrypi
+sudo nano /etc/hostname # same
+
+sudo /etc/init.d/hostname.sh # commit changes
+
+sudo reboot # reboot to take effect
